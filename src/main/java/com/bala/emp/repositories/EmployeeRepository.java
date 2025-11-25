39bc3ba08854059c9ae5d04cc577ef148c5dd4fc
unknown <balasureshkrishna.kodavali@gmail.com>
2025-11-25 10:01:37 +0530

    added code for search employees by department

--- src/main/java/com/bala/emp/repositories/EmployeeRepository.java
+++ src/main/java/com/bala/emp/repositories/EmployeeRepository.java
@@ -3,5 +3,8 @@ package com.bala.emp.repositories;
 import com.bala.emp.entities.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+    List<Employee> findByDepartment(String department);
 }
